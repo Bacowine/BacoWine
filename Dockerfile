2ebbf9dee5f3da7040aa2901e7236ef5d61a3910
Tiberius <vroman01@ucm.es>
2022-03-02 20:42:44 +0100

    DOCKER

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:latest
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+COPY "package*.json" "./"
+
+RUN npm install --production
+
+COPY . .
+
+CMD [ "node", "app.js" ]
--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 "use strict";
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
     BDconfig: {
         host: "localhost",
-        user: "root",
-        password: "12345678",
-        database: "BacoWine"
+        user: process.env.MYSQLDB_USER,
+        password: process.env.MYSQLDB_ROOT_PASSWORD,
+        database: process.env.MYSQLDB_DATABASE
     }
     //port: process.env.PORT || 3000
 }
