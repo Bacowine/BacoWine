7cd19fb6df18b68c31c081d2d945824a181ace39
Pablo López Martín <lopezmartinpablo@gmail.com>
2022-03-04 12:38:41 +0100

    Solucionados cambios con confg.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 "use strict";
 module.exports = {
     BDconfig: {
-        host: "localhost",
-        user: "root",
-        password: "12345678",
-        database: "BacoWine"
+        host: process.env.MYSQLDB_HOST || "localhost",
+        user: process.env.MYSQLDB_USER || "root",
+        password: process.env.MYSQLDB_ROOT_PASSWORD,
+        database: process.env.MYSQLDB_DATABASE
     }
     //port: process.env.PORT || 3000
 }
