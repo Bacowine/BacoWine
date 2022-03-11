cdc975bd9b547a9486f693344d33be727f85ae18
NeutralSwiss <alejandrolaprea@gmail.com>
2022-03-11 11:06:27 +0100

    Cambiado el puerto de la BD a 3307

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ module.exports = {
         host: process.env.MYSQLDB_HOST || "localhost",
         user: process.env.MYSQLDB_USER || "root",
         password: process.env.MYSQLDB_ROOT_PASSWORD,
-        database: process.env.MYSQLDB_DATABASE
+        database: process.env.MYSQLDB_DATABASE,
+        port: process.env.PORT || 3307
     }
-    //port: process.env.PORT || 3000
+
 }
