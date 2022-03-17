a36827ae686bd6202e02addbe40477cdd10841a8
Tiberius <vroman01@ucm.es>
2022-03-17 13:52:44 +0100

    BAC-84 Crear tabla vinos

---
+++ mysql-init-files/schema.sql
@@ -0,0 +1,21 @@
+use bacowine;
+
+--PROTOTIPO
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(45) NOT NULL,
+    correos VARCHAR(45) NOT NULL
+);
+
+--APLICACION
+CREATE TABLE vino (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(45) NOT NULL,
+    clase VARCHAR(45) NOT NULL,
+    tipo VARCHAR(45) NOT NULL, 
+    graduacion DECIMAL(4,2) UNSIGNED NOT NULL CHECK (graduacion <= 100), 
+    bodega VARCHAR(100) NOT NULL, 
+    localidades VARCHAR(500) NOT NULL,
+    foto BLOB,
+    activo BOOLEAN DEFAULT true NOT NULL
+);
