a5bdc31e604df80e40fd5653e73220ca5ed15c73
Tiberius <vroman01@ucm.es>
2022-03-17 15:54:26 +0100

    BAC-90 hacer-vista-router-y-views
    Añadidos datos de prueba a la vista hasta que se haga el controller

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 require('dotenv').config({path: __dirname + '/.env'});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bodegaRouter = require('./routes/bodega');
 
 var fs = require('fs');
 var https = require('https');
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bodega', bodegaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
