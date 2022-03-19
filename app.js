12c4c48a58395c26838094ecf717312d8a8d91b5
Tiberius <vroman01@ucm.es>
2022-03-19 14:05:44 +0100

    BAC-87 Test Unitarios y Refactor
    -Test unitario simple de la raiz de la web
    -Refactorización, separando la creacion del servidor de express en
    bin/www
    -Movidas las variables de entorno a config.js

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config({path: __dirname + '/.env'});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var vinosRouter = require('./routes/vinos');
 
-var fs = require('fs');
-var https = require('https');
 var app = express();
 
 
@@ -43,25 +40,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-if(fs.existsSync('/etc/letsencrypt/live/bacowine.live/fullchain.pem') && fs.existsSync('/etc/letsencrypt/live/bacowine.live/privkey.pem')) {
-	https.createServer({
-	  cert: fs.readFileSync('/etc/letsencrypt/live/bacowine.live/fullchain.pem'),
-	  key: fs.readFileSync('/etc/letsencrypt/live/bacowine.live/privkey.pem')
-	 },app).listen(443, function(){
-		console.log('Servidor https corriendo en el puerto 443');
-	});
-};
-
-//LANZAMIENTO DEL SERVIDOR
-const PORT = process.env.PORT || 80;
-app.listen(PORT, function(err){
-  if(err){ 
-    console.log("No se pudo inicializar el servidor" + err.message);
-  }else{
-    console.log(`Servidor arrancado en el puerto ${PORT}`);
-  } 
 });
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 "use strict";
+require('dotenv').config({path: __dirname + '/.env'});
 module.exports = {
     BDconfig: {
         host: process.env.MYSQLDB_HOST || "localhost",
         user: process.env.MYSQLDB_USER || "root",
         password: process.env.MYSQLDB_ROOT_PASSWORD,
         database: process.env.MYSQLDB_DATABASE,
-        port: process.env.PORT || 3307
+        port: process.env.MYSQLDB_PORT || 3307
+    },
+    App: {
+        port: process.env.NODE_PORT
     }
-
 }
