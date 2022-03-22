322c46cf14c93a8a39b48355cbd76da1334a0a01
Tiberiuss <vroman01@ucm.es>
2022-03-22 15:48:35 +0100

    Env file

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '/.env') });
-
-module.exports = {
-  BDconfig: {
-    host: process.env.MYSQLDB_HOST || 'localhost',
-    user: process.env.MYSQLDB_USER || 'root',
-    password: process.env.MYSQLDB_ROOT_PASSWORD,
-    database: process.env.MYSQLDB_DATABASE,
-    port: process.env.MYSQLDB_PORT || 3307,
-  },
-  App: {
-    port: process.env.NODE_PORT,
-  },
+const BDconfig = {
+  host: process.env.MYSQLDB_HOST,
+  user: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE,
+  port: process.env.MYSQLDB_PORT,
 };
+const App = {
+  port: process.env.NODE_PORT,
+};
+
+const checkA = Object.keys(BDconfig).every((key) => BDconfig[key] !== undefined && BDconfig[key] !== '');
+const checkB = Object.keys(App).every((key) => App[key] !== undefined && App[key] !== '');
+if (!checkA || !checkB) throw new Error('Enviroment not set, check .env file.');
+
+module.exports = { BDconfig, App };
--- mysql-init-files/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'user'@'%' IDENTIFIED BY 'bacowine';
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
-CREATE DATABASE bacowine;
