b4e8c69c9ca68657807d8aec6dc6af384a5d647a
Tiberiuss <vroman01@ucm.es>
2022-03-23 21:41:13 +0100

    Arregladas ruta al detalle del vino
    Debido a confusion con dos archivos uno en singulaar y otro plurarl vino y vinos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var vinoRouter = require('./routes/vino');
+var vinoRouter = require('./routes/vinos');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/vino', vinoRouter);
+app.use('/vinos', vinoRouter);
 
 
 
--- routes/vinos.js
+++ routes/vinos.js
@@ -3,6 +3,14 @@ const cVinos = require('../controllers/controllerVinos');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('jjsaasjasj que pro soy mira');
+});
+
+router.get('/agregarVino', (req, res) => {
+  res.render('agregarVino');
+});
+
 router.get('/mostrarVino', cVinos.verVino);
 
 module.exports = router;
