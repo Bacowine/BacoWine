b0d9f78f8d53d7c381d2b3ffe21c1b637e9fa887
Fernando <fertena@ucm.es>
2022-03-24 12:52:28 +0100

    BAC-112: Formulario agregar bodega
    Pequeños cambios

--- routes/bodega.js
+++ routes/bodega.js
@@ -7,6 +7,9 @@ router.get('/', (_req, res) => {
   res.send('VISTA PRINCIPAL');
 });
 
+router.get('/agregarBodega', (_req, res) => {
+    res.render('agregarBodega', { title: 'BacoWine DEV' });
+  });
 router.get('/insertarBodega', controllerBodega.insertarBodega);
 
 module.exports = router;
