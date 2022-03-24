d058a404ec289779818d60ffa96e0eea479cf6eb
Nitoreto <lei.lioret@gmail.com>
2022-03-24 20:44:25 +0100

    Tarea BAC-105: Funcion del modelo añadir vino.

--- models/modelVinos.js
+++ models/modelVinos.js
@@ -1,11 +1,22 @@
 const pool = require('./db');
 
 const modelVinos = {};
+
 modelVinos.find = async (id) => {
   const sql = pool.format('SELECT * FROM vino where id = ?', [id]);
   const [result] = await pool.promise().query(sql);
   console.log(sql);
   return result;
 };
+
+modelVinos.insert = async (nombre, clase, tipo, graduacion, bodega, localidades, foto) => {
+  const sql = pool.format(
+      'INSERT INTO usuarios(nombre, clase, tipo, graduacion, bodega, localidades, foto, activo) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+        [nombre, clase, tipo, graduacion, bodega, localidades, foto, 1]);
+  const [result] = await pool.promise().query(sql);
+  console.log(sql);
+  return result;
+};
+
 
 module.exports = modelVinos;
