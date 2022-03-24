578dc8b3f2ffeebc8f34296b6a6334071cd64148
Tiberiuss <vroman01@ucm.es>
2022-03-24 18:04:56 +0100

    BAC-114 ControllerBodega y refactor

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var bodegaRouter = require('./routes/bodega');
-var vinosRouter = require('./routes/vinos');
-var bodegaRouter = require('./routes/bodega');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const multer = require('multer');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const bodegaRouter = require('./routes/bodega');
+const vinosRouter = require('./routes/vinos');
 
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-if(app.get('env') !== 'test') app.use(logger('dev'));
+if (app.get('env') !== 'test') app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(multer().any());
@@ -28,15 +27,13 @@ app.use('/bodega', bodegaRouter);
 app.use('/vinos', vinosRouter);
 app.use('/bodega', bodegaRouter);
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((_req, _res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
