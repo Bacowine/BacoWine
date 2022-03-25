2220f74b8fc49bfbf7b397b06949d17f1d2ece1f
Tiberiuss <vroman01@ucm.es>
2022-03-25 23:24:10 +0100

    BAC-122 Separar vistas, añadir cabecera Tambien arreglada ruta del favicon y añadida compresion

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var bodegaRouter = require('./routes/bodega');
-var vinosRouter = require('./routes/vinos');
+const createError = require('http-errors');
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const bodegaRouter = require('./routes/bodega');
+const vinosRouter = require('./routes/vinos');
 
-var app = express();
-
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-if(app.get('env') !== 'test') app.use(logger('dev'));
+app.use(compression());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+if (app.get('env') !== 'test') app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/bodega', bodegaRouter);
 app.use('/vinos', vinosRouter);
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
