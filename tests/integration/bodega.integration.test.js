189f32a35fef451e5cad1796b24ec7ec34f40379
NeutralSwiss <alejandrolaprea@gmail.com>
2022-03-25 12:37:44 +0100

    BAC-115 Arreglados los test unitarios de agregar bodega

--- tests/integration/bodega.integration.test.js
+++ tests/integration/bodega.integration.test.js
@@ -2,43 +2,49 @@ const pool = require('../../models/db');
 const modelBodega = require('../../models/modelBodega');
 
 describe('agregar bodega', () => {
-    afterAll(() => {
-      pool.end();
-    });
+  afterAll(() => {
+    pool.end();
+  });
 
-    test('agregar bodega con alfanumericos', async () => {
-        const [result] = await modelBodega.add('Azpilicueta', 1929, 'Rioja', 'Muy bonito', 'Rioja', null);
+  test('agregar bodega con alfanumericos', async () => {
+    let result;
+    let err;
 
-        //expect(result).not.toBeNaN();
-        expect(result).toEqual(expect.any(Number));
-    });
+    try {
+      result = await modelBodega.add(['Azpilicueta', 1929, 'Rioja', 'Muy bonito', 'Rioja', 0]);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      err = e;
+    }
+    expect(result).toEqual(expect.any(Number));
+    expect(err).toBe(undefined);
+  });
 
-    test('agregar bodega con anyo incorrecto', async () => {
+  test('agregar bodega con anyo incorrecto', async () => {
+    let result;
+    let err;
 
+    try {
+      result = await modelBodega.add(['Azpilicueta', '*&*&(', 'Rioja', 'Muy bonito', 'Rioja', 0]);
+    } catch (e) {
+      err = e;
+    }
+    expect(result).toBe(undefined);
+    expect(err).toBeInstanceOf(Error);
+  });
 
-        try {
-            const [result] = await modelBodega.add('Azpilicueta', '*&*&(', 'Rioja', 'Muy bonito', 'Rioja', null);
-        }
-        catch(e) {
-            err = e;
-        }
-        expect(result).toBe(undefined);
-        expect(err).toBe(undefined);
-    });
+  test('agregar bodega con valores no alfanumericos', async () => {
+    let result;
+    let err;
 
-    test('agregar bodega con valores no alfanumericos', async () => {
-        let result;
-        let err;
-        
-        try {
-            [result] = await modelBodega.add('+*-', ')&*^', '*/*-*', '+++', '@#$', null);
-        }
-        catch(e) {
-            err = e;
-        }
-       
-        expect(result).toBe(undefined);
-        expect(err).toBe(undefined);
-    });
+    try {
+      result = await modelBodega.add(['+*-', ')&*^', '*/*-*', '+++', '@#$', 0]);
+    } catch (e) {
+      err = e;
+    }
 
+    expect(result).toBe(undefined);
+    expect(err).toBeInstanceOf(Error);
+  });
 });
