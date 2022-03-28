2e8d07a877de57c59cd1716cf17cb874099427c1
Fernando <fertena@ucm.es>
2022-03-28 16:21:01 +0200

    BAC-131: Vincular vista con rutas

--- routes/bodega.js
+++ routes/bodega.js
@@ -12,5 +12,6 @@ router.get('/', (_req, res) => res.redirect('/bodega/agregarBodega'));
 router.get('/detalles', controllerBodega.mostrarDetallesBodega);
 router.get('/agregarBodega', (_req, res) => res.render('agregarBodega'));
 router.post('/agregarBodega', upload.single('imagen'), validate(bodegaSchema), controllerBodega.insertarBodega);
+router.get('/borrarBodega', controllerBodega.borrarBodega);
 
 module.exports = router;
