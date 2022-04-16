25f7b85b76c0a7e97d29093b8d205a9b9aa02009
Nitoreto <lei.lioret@gmail.com>
2022-04-16 20:14:49 +0200

    Añadido funcion valorarVino, modificarvalorarVino, buscarValoracionVino y modificado el insertar

--- models/modelVino.js
+++ models/modelVino.js
@@ -30,7 +30,7 @@ modelVino.find = async (id) => {
   }
 };
 
-modelVino.insert = async (rows, variedad) => {
+modelVino.insert = async (rows, variedad, valoracion) => {
   let conn;
   try {
     conn = await pool.promise().getConnection();
@@ -38,6 +38,8 @@ modelVino.insert = async (rows, variedad) => {
     const sql1 = pool.format('INSERT INTO vino(nombre, clase, tipo, maceracion, graduacion, bodega, localidades, foto) VALUES(?)', [rows]);
     console.log(sql1.substring(0, 500));
     const [result] = await conn.query(sql1);
+    
+    const sql2 = pool.format('INSERT INTO valoracion_vino(vino, valoracion) VALUES(?,?)',[rows[0],valoracion]);
 
     await Promise.all(Object.entries(variedad).map(async ([key, value]) => {
       const sql2 = pool.format('INSERT INTO variedad_vino(vino,nombre_variedad,porcentaje) VALUES(?,?,?)', [result.insertId, key, value]);
@@ -92,6 +94,27 @@ modelVino.buscarComentario = async (id) => {
 
 modelVino.borrarComentario = async (id) => {
   const sql = pool.format('DELETE FROM comentario WHERE id = ?', [id]);
+  const [result] = await pool.promise().query(sql);
+  console.log(result);
+  return result;
+};
+
+modelVino.valorarVino = async (rows) => {
+  const sql = pool.format('INSERT INTO valoracion_vino(vino,valoracion) VALUES(?)', [rows]);
+  const [result] = await pool.promise().query(sql);
+  console.log(sql);
+  return result.insertId;
+};
+
+modelVino.modificarvalorarVino = async (id, valoracion) => {
+  const sql = pool.format('UPDATE valoracion_vino SET valoracion = ? where vino = id ', [valoracion, id]);
+  const [result] = await pool.promise().query(sql);
+  console.log(sql);
+  return result.insertId;
+};
+
+modelVino.buscarValoracionVino= async (id) => {
+  const sql = pool.format('SELECT * FROM valoracion_vino WHERE vino = ?', [id]);
   const [result] = await pool.promise().query(sql);
   console.log(result);
   return result;
