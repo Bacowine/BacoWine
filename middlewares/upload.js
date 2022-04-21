f79cb21fecf17a826ec099b49cb86117608568fa
Tiberiuss <vroman01@ucm.es>
2022-04-21 14:35:23 +0200

    BAC-259 Middleware imagenes

---
+++ middlewares/upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
+const upload = multer({
+  limits: {
+    fileSize: 4 * 1024 * 1024, // 4MiB
+  },
+});
+
+const imageHandler = async (req, res, next) => {
+  if (req.file) {
+    try {
+      req.file.buffer = await sharp(req.file.buffer)
+        .resize({
+          width: 1024,
+          height: 768,
+          fit: 'contain',
+          background: {
+            r: 0, g: 0, b: 0, alpha: 0,
+          },
+        })
+        .webp({ nearLossless: true, quality: 50, alphaQuality: 100 })
+        .toBuffer();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  next();
+};
+
+module.exports = { upload, imageHandler };
--- routes/bodega.js
+++ routes/bodega.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const multer = require('multer');
 
-const upload = multer();
+const { upload, imageHandler } = require('../middlewares/upload');
 const controllerBodega = require('../controllers/controllerBodega');
 const bodegaSchema = require('../validators/bodega.validator');
 const validate = require('../middlewares/schemaValidator');
@@ -18,7 +17,7 @@ router.route('/detalles')
 
 router.route('/agregarBodega')
   .get(authRole(ROLE.ADMIN), (_req, res) => res.render('agregarBodega'))
-  .post(authRole(ROLE.ADMIN), upload.single('imagen'), validate(bodegaSchema), controllerBodega.insertarBodega);
+  .post(authRole(ROLE.ADMIN), upload.single('imagen'), imageHandler, validate(bodegaSchema), controllerBodega.insertarBodega);
 
 router.route('/borrarBodega')
   .post(authRole(ROLE.ADMIN), controllerBodega.borrarBodega);
